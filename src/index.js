6d937866a42e6934f592b3cb2c0bfe2dd11cac64
kev-69 <bismarkobuobi19@gmail.com>
2025-04-12 16:51:55 +0000

    Refactor index.js to organize route imports and fix console log typo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const env = require('./config/env');
 
+// import routes
+const projectRoutes = require('./routes/routes');
+const errorHandler = require('./middlewares/errorHandler');
+
 const app = express();
 const PORT = env.PORT || 3000;
 const HOST = env.HOST || 'localhost';
@@ -9,12 +13,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Import routes
-
+app.use('/api/projects', projectRoutes); // routes for projects
 
 // error handling middleware
-
+app.use(errorHandler)
 
 // start server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://${HOST}:${POST}`);
+    console.log(`Server is running on http://${HOST}:${PORT}`);
 })
